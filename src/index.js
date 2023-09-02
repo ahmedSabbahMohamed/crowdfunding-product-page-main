12402452c0377859ca15fbdf099fe9f222e815f3
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2023-09-02 10:48:32 +0300

    start challenge

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// js file from bootstrap
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js"
+// css file from bootstrap
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
